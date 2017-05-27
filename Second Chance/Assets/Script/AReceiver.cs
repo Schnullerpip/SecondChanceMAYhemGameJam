a91c045cf91326250db0acb1e1f026c70ebb0434
Julian <Schnullerpip@gmail.com>
2017-05-27 11:54:22 +0200

    cleaned up door

--- Second Chance/Assets/Script/AReceiver.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AReceiver : MonoBehaviour
-{
-    //the generic method that is trigered by a receiver, that knows this receiver
-    public abstract void ActOnReceive();
-}
