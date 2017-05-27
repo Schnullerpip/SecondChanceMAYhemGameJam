a3842c6e198e206c1f92ab53a5bbea5a220260e3
Julian <Schnullerpip@gmail.com>
2017-05-27 11:56:16 +0200

    moved scripts to Scripts from Script

---
+++ Second Chance/Assets/Scripts/DoorReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorReceiver : AReceiver
+{
+
+    //MEMBER
+    //the doors Animator
+    private Animator m_Animator;
+
+    //METHODS
+    public override void ActOnReceive()
+    {
+        m_Animator.SetBool("shouldOpen", true);
+    }
+
+
+    //INHERITED METHODS
+	// Use this for initialization
+	void Start () {
+		//get the gameobjects transform
+	    m_Animator = gameObject.GetComponent<Animator>();
+	}
+
+}
---
+++ Second Chance/Assets/Scripts/tmp_playerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmp_playerScript : MonoBehaviour
+{
+
+    private Sender m_Sender;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    m_Sender = gameObject.GetComponent<Sender>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        if (m_Sender == null)
+	        {
+                Debug.Log("m_Sender was not found!");
+	            return;
+	        }
+            m_Sender.TriggerReceivers();
+	    }
+	}
+}
