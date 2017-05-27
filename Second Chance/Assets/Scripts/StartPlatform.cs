ca42fb69a501de2d356165584b9e730439d00965
Julian <Schnullerpip@gmail.com>
2017-05-27 20:30:03 +0200

    created triggerplatform that is a sender

---
+++ Second Chance/Assets/Scripts/StartPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPlatform : MonoBehaviour
+{
+
+    private Sender m_Sender;
+    private bool alreadyLeft = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    m_Sender = GetComponentInParent<Sender>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        m_Sender.TriggerReceivers();
+        Destroy(this);
+    }
+}
